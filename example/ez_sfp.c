fd7a5b0a23e33e4694078e48a0e486496f308cbf
Nicholas Zamora <nzamora@mtu.edu>
2025-11-18 19:55:17 -0500

    Set GPIO G and enable SFP power

--- example/ez_sfp.c
+++ example/ez_sfp.c
@@ -16,17 +16,34 @@ int main() {
 	if (found_devices_count == 0) {
 		return -1;
 	}
-	libredxx_opened_device* opened = NULL;
-	status = libredxx_open_device(found_devices[0], &opened);
+	libredxx_opened_device* device = NULL;
+	status = libredxx_open_device(found_devices[0], &device);
 
 	struct libredxx_ft260_feature_out_report rep_ftr_out;
+	size_t size = 0;
 
 	// set GPIO G function
 	memset(&rep_ftr_out, 0, sizeof(rep_ftr_out));
+	size = sizeof(rep_ftr_out);
 	rep_ftr_out.report_id = 0xA1;
 	rep_ftr_out.gpio_function.request = 0x09;
 	rep_ftr_out.gpio_function.function = 0;
+	status = libredxx_write(device, rep_ftr_out.bytes, &size, LIBREDXX_ENDPOINT_FEATURE);
 
+	// set GPIO G direction
+	memset(&rep_ftr_out, 0, sizeof(rep_ftr_out));
+	size = sizeof(rep_ftr_out);
+	rep_ftr_out.report_id = 0xB0;
+	rep_ftr_out.gpio_write.gpio_dir_ex = 1 << 6;
+	status = libredxx_write(device, rep_ftr_out.bytes, &size, LIBREDXX_ENDPOINT_FEATURE);
 
+	// set GPIO value
+	memset(&rep_ftr_out, 0, sizeof(rep_ftr_out));
+	size = sizeof(rep_ftr_out);
+	rep_ftr_out.report_id = 0xB0;
+	rep_ftr_out.gpio_write.gpio_dir_ex = 1 << 6;
+	rep_ftr_out.gpio_write.gpio_val_ex = 1 << 6;
+	status = libredxx_write(device, rep_ftr_out.bytes, &size, LIBREDXX_ENDPOINT_FEATURE);
 
+	return 0;
 }
