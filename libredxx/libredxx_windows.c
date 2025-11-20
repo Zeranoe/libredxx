54d4bb279c61e4b1859de638d82d74bd6c4ee99d
Nicholas Zamora <nzamora@mtu.edu>
2025-11-19 20:08:54 -0500

    Updates to interrupts

--- libredxx/libredxx_windows.c
+++ libredxx/libredxx_windows.c
@@ -253,7 +253,7 @@ static libredxx_status libredxx_d3xx_set_timeout(libredxx_opened_device* device,
 
 libredxx_status libredxx_open_device(const libredxx_found_device* found, libredxx_opened_device** opened)
 {
-	DWORD create_flags = found->type == LIBREDXX_DEVICE_TYPE_D2XX || found->type == LIBREDXX_DEVICE_TYPE_FT260
+	DWORD create_flags = found->type == LIBREDXX_DEVICE_TYPE_D2XX
 		                     ? 0
 		                     : FILE_FLAG_OVERLAPPED | FILE_ATTRIBUTE_NORMAL;
 	HANDLE handle = CreateFileW(found->path, GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, create_flags, NULL);
@@ -336,19 +336,25 @@ static libredxx_status libredxx_d3xx_set_stream_pipe(libredxx_opened_device* dev
 libredxx_status libredxx_interrupt(libredxx_opened_device* device)
 {
 	device->read_interrupted = true;
+	libredxx_status status = LIBREDXX_STATUS_SUCCESS;
+
 	if (device->found.type == LIBREDXX_DEVICE_TYPE_D2XX) {
-		return SetEvent(device->d2xx_read_event) ? LIBREDXX_STATUS_SUCCESS : LIBREDXX_STATUS_ERROR_SYS;
+		if (!SetEvent(device->d2xx_read_event)) {
+			status = LIBREDXX_STATUS_ERROR_SYS;
+		}
 	} else if (device->found.type == LIBREDXX_DEVICE_TYPE_D3XX) {
 		// abort also released the overlapped event
-		libredxx_status status;
 		status = libredxx_d3xx_abort_pipe(device, 0x82);
 		if (status != LIBREDXX_STATUS_SUCCESS) {
 			return status;
 		}
 		status = libredxx_d3xx_abort_pipe(device, 0x02);
-		return status;
+	} else if (device->found.type == LIBREDXX_DEVICE_TYPE_FT260) {
+		if (!CancelIoEx(device->handle, NULL)) {
+			status = LIBREDXX_STATUS_ERROR_SYS;
+		}
 	}
-	return LIBREDXX_STATUS_SUCCESS; // TODO FT260?
+	return status;
 }
 
 static libredxx_status libredxx_d2xx_rx_available(libredxx_opened_device* device, size_t* available)
@@ -464,13 +470,8 @@ libredxx_status libredxx_read(libredxx_opened_device* device, void* buffer, size
 				if (GetLastError() != ERROR_IO_PENDING) {
 					ret = LIBREDXX_STATUS_ERROR_SYS;
 				} else if (!GetOverlappedResult(device->handle, &overlapped, (DWORD*)buffer_size, true)) {
-					ret = LIBREDXX_STATUS_ERROR_SYS;
+					ret = (GetLastError() == ERROR_OPERATION_ABORTED && device->read_interrupted) ? LIBREDXX_STATUS_ERROR_INTERRUPTED : LIBREDXX_STATUS_ERROR_SYS;
 				}
-			}
-			if (ret == LIBREDXX_STATUS_SUCCESS && bBuffer[0] != report_id) {
-				// all reads should be triggered be synchronized with a read request report
-				// this could be interrupted by GPIO interrupt (unsupported)
-				ret = LIBREDXX_STATUS_ERROR_IO;
 			}
 			CloseHandle(overlapped.hEvent);
 		}
